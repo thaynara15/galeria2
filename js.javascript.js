7bc9b0ae00a4523f3a577697a66dc3414ab1b19a
Thaynara <104026459+thaynara15@users.noreply.github.com>
2023-02-16 15:38:45 -0300

    Add files via upload

---
+++ js.javascript.js
@@ -0,0 +1,49 @@
+var gallery = [
+    {
+      src: "https://lipsum.app/id/61/1800x1200",
+      thumb: "https://lipsum.app/id/2/80x80",
+      caption: "First image",
+    },
+    {
+      src: "https://lipsum.app/id/3/800x600",
+      thumb: "https://lipsum.app/id/3/80x80",
+      caption: "Second image",
+    },
+    {
+      src: "https://lipsum.app/id/4/800x600",
+      thumb: "https://lipsum.app/id/4/80x80",
+      caption: "Third image",
+    },
+    {
+      src: "https://lipsum.app/id/4/800x600",
+      thumb: "https://lipsum.app/id/4/80x80",
+      caption: "Third image",
+    },
+
+    <head>
+        <body>
+        <div id= "seta"></div>
+    </body>
+    </head>
+
+    
+  ];
+
+  #moldura{
+    width:600px;                                           
+    height:450px;                                
+    transition:background-image 2s; 
+     background-repeat: no-repeat;           
+    background-size:100%;                         
+    }
+    
+    #seta {
+        position: absolute;
+        padding: 20px;
+        box-shadow: 2px -2px 0 1px #000 inset;
+        border: solid transparent;
+        border-width: 0 0 2px 2px;
+        transform: rotate(45deg);
+      }
+
+  
